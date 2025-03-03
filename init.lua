86bd5c07e79c9575def49a21f443fcd77e7b065c
Todor Dimitrov <dimitrov@technology.de>
2025-03-03 14:51:57 +0100

    Sets up JSON formatter

--- init.lua
+++ init.lua
@@ -807,6 +807,7 @@ require('lazy').setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
         vue = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'fixjson', stop_after_first = true },
       },
     },
   },
